3c1e70e35beeda80372f702c2c9a2a7633a9877f
fxbrazier <fx.brazier@gmail.com>
2018-05-17 16:03:30 +0200

    TwitterOAuth

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,22 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/twitter", name="get_twitter")
+     */
+    public function getTwitterAction(Request $request)
+    {
+        $connection = new TwitterOAuth('c3xA7ljueARGf2xotSnE7Zpiq', '8B167u7wLDoDL4JHOgbTrUcoE8wGlOMApCY1aqFs2jqvaKW9Qo', '549983690-9hBiemayMMuZDyuqRkW8XbGBojQemWLvAz5J0DDF', 'aeLtcCDVoiGz0ctgGMxQSsyqTW7QMqT5JLAGniWnOUaTj');
+        $statuses = $connection->get("search/tweets", ["q" => "football"]);
+        dump($statuses);die;
+
+
+
+        // replace this example code with whatever you need
+        /*return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);*/
     }
 }
